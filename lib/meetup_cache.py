b30aa465e3f966e8cd5aa770e0a73a327738df69
Edan Elazar <edan.y.elazar@gmail.com>
2022-02-26 17:44:26 -0600

    meetup_cache functions

--- lib/meetup_cache.py
+++ lib/meetup_cache.py
@@ -9,3 +9,22 @@ class MeetupCache:
 
     def get_meetup(self, id):
         return next(filter(lambda x: x.id == id, self.meetups), None)
+
+    def get_user_meetups(self, attendee):
+        meetup_ids = []
+        for event in self.meetups:
+            if attendee in event.attendees:
+                meetup_ids.append(event)
+        return meetup_ids
+
+    # assumes attendee has a latitude and longitude
+    def get_available_meetups(self, attendee):
+        available_meetups = []
+        current_meetups = get_attendee_meetups(self, attendee)
+        current_times = []
+        for event in current_meetups:
+            current_times.append(event.start)
+        distance = event
+        for event in self.meetups:
+            if event not in current_meetups and event.time not in current_times:
+                
