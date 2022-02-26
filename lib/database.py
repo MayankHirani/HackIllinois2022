6c16e559aa66fab5855277903b0a8db0b5b5271d
akaashdash <akaash.dash@gmail.com>
2022-02-26 17:29:42 -0600

    added database

---
+++ lib/database.py
@@ -0,0 +1,16 @@
+import json
+
+from lib.restaurant import Restaurant
+from .location import Location
+# Database controller
+
+# File string
+class RestaurantDatabase():
+    def __init__(self, path) -> None:
+        file = open(path)
+        data = json.load(file)
+        self.restaurants = []
+        for restaurant_obj in data["data"]:
+            location = Location(restaurant_obj["address"]["formatted"], restaurant_obj["geo"]["lat"], restaurant_obj["geo"]["lon"])
+            restaurant = Restaurant(str(restaurant_obj["restaurant_id"]), restaurant_obj["restaurant_name"], location)
+            self.restaurants.append(restaurant)
