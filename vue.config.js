8595a7f42939ced5dd9ab9e276f65acb6388644b
MayankHirani <mayank.hirani@icloud.com>
2022-02-26 23:05:18 -0600

    Add disableHostCheck for Heroku

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    compress: true,
+    disableHostCheck: true,
+    allowedHosts: [
+        '.heroku.com', '.herokuapps.com'
+    ]
+  }
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
-module.exports = {
-    devServer: {
-        compress: true,
-        disableHostCheck: true,
-        allowedHosts: [
-            '.heroku.com', '.herokuapps.com'
-        ]
-    }
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: [
+    'vuetify'
+  ],
+  devServer: {
+    compress: true,
+    disableHostCheck: true,
+    allowedHosts: [
+        '.heroku.com', '.herokuapps.com'
+    ]
+  }
+})
